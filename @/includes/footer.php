b2d98606a4758eabc2a9e7b983c42bb59dc218f9
Meet Thakar <mvthakar@outlook.com>
2026-09-08 17:27:03 +0530

    setup: add AdminLTE template

--- @/includes/footer.php
+++ @/includes/footer.php
@@ -1,3 +1,12 @@
+        </div>
+    </section>
+  </div>
+  <!-- /Content Wrapper -->
+</div>
+<!-- /wrapper -->
 
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
 </body>
 </html>
