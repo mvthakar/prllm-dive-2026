d0e75504ec5c59efaffaeb339c0a473afecd38ac
Meet Thakar <mvthakar@outlook.com>
2026-09-08 16:47:53 +0530

    setup: separate header and footer

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Welcome</h1>   
-</body>
-</html>
+<?php require_once './@/lib/init.php'; ?>
+<?php require_once pathOf('/@/includes/header.php'); ?>
+
+<h1>Welcome</h1>
+
+<?php require_once pathOf('/@/includes/footer.php'); ?>
